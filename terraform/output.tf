a3cd25c47ab13be57a87ab4a8c572fe51a6cbfd8
Andrea C. Crawford <acm@us.ibm.com>
2026-01-21 14:16:19 -0500

    fixed type-o in output

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,7 +7,7 @@ output "message" {
     Resource Group: ${data.ibm_resource_group.resource_group.id}
     Region: ${var.region}
     Subnet ID: ${ibm_is_subnet.golden_image_subnet.id}
-    COS Bucket: "${ibm_cos_bucket.cos_bucket.bucket_name}
+    COS Bucket: "${ibm_cos_bucket.cos_bucket.bucket_name}"
 
     If running the Packer golden image automation, be sure to take note of the values above,
     they will need to be added to the `variables.pkvars.hcl` file
